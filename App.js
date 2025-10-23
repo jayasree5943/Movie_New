b47a99c03ff6857037faae8810b90b9c915124f6
Jaya sree Yandrapu <117093033+jayasree5943@users.noreply.github.com>
2025-10-22 22:57:48 -0700

    App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import Header from './src/components/Header.js/Header';
 import Body from './src/components/Body.js/Body';
 import Footer from './src/components/Footer.js/Footer';
+import './index.css';
 
 var reactRoot = createRoot(document.getElementById('root'));
 
